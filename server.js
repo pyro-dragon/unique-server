85ab3b8267ce25103c8e5dfe3bc5c0b8b9ff3f29
Pyro-dragon <pyro@dragonscancode.com>
2016-10-09 19:11:25 +0100

    Replacing with better code

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var fs = require("fs");
+
+// Configure the app to use bodyParser()
+// This lets us get data from a POST request
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Set the application port
+var port = process.env.PORT || 8080;
+
+// Routes for the API
+
+var router = express.Router();   // The express router
+
+// Test to make sure everything is working
+router.get("/", function(request, response)
+{
+   request.json({
+      message: "Horray! Welcome to our API"
+   });
+});
+
+// Start the server
+app.listen(port);
+console.log("Magic happens on port " + port);
+
+// Register the routes
+// All of the routes will be prefixed with /api
+/*app.use("/api", router);
 
 // GET list of all users
 app.get('/listUsers', function (req, res)
@@ -61,4 +90,4 @@ var server = app.listen(8081, function ()
 	var port = server.address().port;
 
 	console.log("Example app listening at http://%s:%s", host, port)
-});
+});*/
